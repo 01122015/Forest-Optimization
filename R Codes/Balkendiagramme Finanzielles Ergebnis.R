adfd74bd047fd4a12e5ee202ceebaee6a79707c6
Messerer <gu95ner@mytum.de>
2019-08-28 13:38:25 +0200

    First upload

---
+++ R Codes/Balkendiagramme Finanzielles Ergebnis.R
@@ -0,0 +1,7 @@
+library(readxl)
+Abfrage_Finanzielles_Ergebnis_2000_2050_2070 <- read_excel("~/a SURVIVAL KW/Daten/YAFO Ergebnisse_nue 1%/Data_R_fin.xlsx")
+
+barplot(table(data(BetriebID;)),col=c("green","orange","red"),
+        
+        ylab="Annuität")
+
